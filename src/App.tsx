0b79a6b05e0ac6e101666e555ad0972f58771b51
Nyx <nyxfor13days@gmail.com>
2022-12-12 13:39:45 +0530

    Added Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+
 export default () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 };
