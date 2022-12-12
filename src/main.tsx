19661fd111f32e40e31a9261c348cefe45b3da74
Nyx <nyxfor13days@gmail.com>
2022-12-12 15:39:54 +0530

    Removed SCSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./stylesheet/main.scss";
+import "./stylesheet/main.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
