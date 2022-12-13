575be36d48a17032403ff89097ed0cbed11b94ea
Nyx <nyxfor13days@gmail.com>
2022-12-13 12:58:04 +0530

    Created Initial UI

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,5 +1,47 @@
+import "./Home.css";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <div className="page">
+        <section className="posts__container">
+          <article className="post">
+            <h3 className="post__title">title</h3>
+            <p className="post__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+              neque tempora! Quod corporis amet deleniti quas, placeat,
+              explicabo commodi est, mollitia rem animi unde? Tenetur,
+              molestias? Eligendi expedita iste vero!
+            </p>
+          </article>
+
+          <article className="post">
+            <h3 className="post__title">title</h3>
+            <p className="post__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+              neque tempora! Quod corporis amet deleniti quas, placeat,
+              explicabo commodi est, mollitia rem animi unde? Tenetur,
+              molestias? Eligendi expedita iste vero!
+            </p>
+          </article>
+
+          <article className="post">
+            <h3 className="post__title">title</h3>
+            <p className="post__description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+              neque tempora! Quod corporis amet deleniti quas, placeat,
+              explicabo commodi est, mollitia rem animi unde? Tenetur,
+              molestias? Eligendi expedita iste vero!
+            </p>
+          </article>
+        </section>
+
+        <aside className="sidebar">
+          <div className="sidebar__container">aside</div>
+        </aside>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
