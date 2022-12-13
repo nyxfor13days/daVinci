da3137c8211a05ccc32f7aac92863eb314f4afac
Nyx <nyxfor13days@gmail.com>
2022-12-13 14:30:45 +0530

    Added Route for Admin Login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,15 @@
 import { lazy } from "react";
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./Home";
 
-export const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
+const Home = lazy(() =>
+  import("./Home").then((module) => ({ default: module.default }))
+);
+
+export const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  {
+    path: "/admin",
+    element: <div>admin login</div>,
+    children: [{ path: "/admin/:id", element: <div>admin panel</div> }],
+  },
+]);
