b7136bf6e153402186a0c7c534b7332b22e7b3bb
Nyx <nyxfor13days@gmail.com>
2022-12-13 14:02:49 +0530

    Setup mock api

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,39 +1,27 @@
+import { useState, useEffect } from "react";
+import { PostT } from "../../types";
 import "./Home.css";
 
 const Home = () => {
+  const [posts, setPosts] = useState<PostT[]>([]);
+
+  useEffect(() => {
+    // use fetch to get first 20 posts from api
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => setPosts(data.slice(0, 20)));
+  }, []);
+
   return (
     <div className="container">
       <div className="page">
         <section className="posts__container">
-          <article className="post">
-            <h3 className="post__title">title</h3>
-            <p className="post__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-              neque tempora! Quod corporis amet deleniti quas, placeat,
-              explicabo commodi est, mollitia rem animi unde? Tenetur,
-              molestias? Eligendi expedita iste vero!
-            </p>
-          </article>
-
-          <article className="post">
-            <h3 className="post__title">title</h3>
-            <p className="post__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-              neque tempora! Quod corporis amet deleniti quas, placeat,
-              explicabo commodi est, mollitia rem animi unde? Tenetur,
-              molestias? Eligendi expedita iste vero!
-            </p>
-          </article>
-
-          <article className="post">
-            <h3 className="post__title">title</h3>
-            <p className="post__description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
-              neque tempora! Quod corporis amet deleniti quas, placeat,
-              explicabo commodi est, mollitia rem animi unde? Tenetur,
-              molestias? Eligendi expedita iste vero!
-            </p>
-          </article>
+          {posts.map((post) => (
+            <article className="post" key={post.id}>
+              <h3 className="post__title">{post.title}</h3>
+              <p className="post__description">{post.body}</p>
+            </article>
+          ))}
         </section>
 
         <aside className="sidebar">
