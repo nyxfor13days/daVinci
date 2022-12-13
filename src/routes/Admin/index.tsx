21cc59cc98deb2bb59e11efd9065a34cd004ceed
Nyx <nyxfor13days@gmail.com>
2022-12-13 15:15:12 +0530

    Added Admin Panel file

---
+++ src/routes/Admin/index.tsx
@@ -0,0 +1,7 @@
+import "./Admin.css";
+
+const AdminLogin = () => {
+  return <div className="container">Admin Login</div>;
+};
+
+export default AdminLogin;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,15 @@ import { createBrowserRouter } from "react-router-dom";
 const Home = lazy(() =>
   import("./Home").then((module) => ({ default: module.default }))
 );
+const AdminLogin = lazy(() =>
+  import("./Admin").then((module) => ({ default: module.default }))
+);
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   {
     path: "/admin",
-    element: <div>admin login</div>,
+    element: <AdminLogin />,
     children: [{ path: "/admin/:id", element: <div>admin panel</div> }],
   },
 ]);
